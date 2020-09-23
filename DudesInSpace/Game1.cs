28496e5249690ffe1bdd7da544db64001565d7d7
Nigel Kennington <nigel.kennington@edinburghcollege.ac.uk>
2020-09-23 23:56:56 +0100

    Initial commit

---
+++ DudesInSpace/Game1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace DudesInSpace
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public static readonly Random RNG = new Random();
+
+        MouseState ms, ms_old;
+
+        private List<Color> paletteList;
+
+        Graphic2D background;
+        List<SpaceDude> Dudes;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            Dudes = new List<SpaceDude>();
+
+            #region Generate some visually distinct colours for the dudes.
+            paletteList = new List<Color>();
+            paletteList.Add(new Color(230, 25, 75, 255)); // Red
+            paletteList.Add(new Color(60, 180, 75, 255)); // Green
+            paletteList.Add(new Color(255, 225, 25, 255));    // Yellow
+            paletteList.Add(new Color(0, 130, 200, 255)); // Blue
+            paletteList.Add(new Color(245, 130, 48, 255));    // Orange
+            paletteList.Add(new Color(145, 30, 180, 255)); // Purple
+            paletteList.Add(new Color(70, 240, 240, 255));    // Cyan
+            paletteList.Add(new Color(240, 50, 230, 255)); // Magenta
+            paletteList.Add(new Color(210, 245, 60, 255));    // Lime
+            paletteList.Add(new Color(250, 190, 190, 255));   // Pink
+            paletteList.Add(new Color(0, 128, 128, 255)); // Teal
+            paletteList.Add(new Color(230, 190, 255, 255));   // Lavender
+            paletteList.Add(new Color(170, 110, 40, 255));    // Brown
+            paletteList.Add(new Color(255, 250, 200, 255));   // Beige
+            paletteList.Add(new Color(128, 0, 0, 255));   // Maroon
+            paletteList.Add(new Color(170, 255, 195, 255));   // Mint
+            paletteList.Add(new Color(128, 128, 0, 255)); // Olive
+            paletteList.Add(new Color(255, 215, 180, 255));   // Apricot
+            paletteList.Add(new Color(0, 0, 128, 255));   // Navy
+            #endregion
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = new Graphic2D(Content.Load<Texture2D>("starfield"));
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Vector2 randomSpeed;
+
+            ms = Mouse.GetState();
+
+            if (ms.LeftButton == ButtonState.Pressed && ms_old.LeftButton == ButtonState.Released)
+            {
+                randomSpeed = new Vector2((float)RNG.NextDouble() - 0.5f, (float)RNG.NextDouble() - 0.5f);
+                Dudes.Add(new SpaceDude(Content.Load<Texture2D>("dudeBody"), Content.Load<Texture2D>("dudeVisor"), paletteList[RNG.Next(paletteList.Count)],
+                                            400, 240, randomSpeed, 0, randomSpeed.Length() / 10f));
+            }
+
+            foreach (var dude in Dudes)
+            {
+                dude.UpdateMe(GraphicsDevice.Viewport.Bounds);
+            }
+
+            ms_old = ms;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+            background.DrawMe(_spriteBatch);
+            foreach (var dude in Dudes)
+            {
+                dude.DrawMe(_spriteBatch);
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ DudesInSpace/SpaceDude.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DudesInSpace
+{
+    class Graphic2D
+    {
+        // Class Variables
+        private Vector2 _pos;
+        protected Texture2D _txr;
+
+        // Class Properties
+        public Vector2 Position
+        {
+            get
+            {
+                return _pos;
+            }
+
+            set 
+            {
+                _pos = value;
+            }
+        }
+
+        // Class Constructors
+        public Graphic2D(Texture2D texture2D, int xPos = 0, int yPos = 0)
+        {
+
+            _pos = new Vector2(xPos, yPos);
+            _txr = texture2D;
+        }
+
+        // Class Methods
+        public virtual void DrawMe(SpriteBatch sBatch)
+        {
+            sBatch.Draw(_txr, _pos, Color.White);
+        }
+
+    }
+
+    class Sprite2D : Graphic2D
+    {
+        private float _rotation;
+        private float _rotSpeed;
+        private Vector2 _rotCenter;
+
+        private Color _tint;
+
+        public Sprite2D(Texture2D tex, int xPos, int yPos, Color tint, float rot, float rotSpeed) : base (tex, xPos, yPos)
+        {
+            _rotation = rot;
+            _rotSpeed = rotSpeed;
+            _rotCenter = tex.Bounds.Center.ToVector2();
+
+            _tint = tint;
+        }
+
+        public override void DrawMe(SpriteBatch sBatch)
+        {
+            _rotation += _rotSpeed;
+
+            sBatch.Draw(_txr, Position, null, _tint, _rotation, _rotCenter, 1, SpriteEffects.None, 0);
+        }
+    }
+
+    class SpaceDude
+    {
+        private Sprite2D _dudeBody;
+        private Sprite2D _dudeVisor;
+
+        private Vector2 _velocity;
+
+        public SpaceDude(Texture2D bodyTex, Texture2D visorTex, Color tint, int xPos, int yPos, Vector2 vel, float rot = 0, float rotSpeed = 0)
+        {
+            _dudeVisor = new Sprite2D(visorTex, xPos, yPos, Color.White, rot, rotSpeed);
+            _dudeBody = new Sprite2D(bodyTex, xPos, yPos, tint, rot, rotSpeed);
+
+            _velocity = vel;
+        }
+
+        public void UpdateMe(Rectangle bounds)
+        {
+            _dudeBody.Position += _velocity;
+            _dudeVisor.Position += _velocity;
+
+            if (_dudeBody.Position.X < bounds.Left || _dudeBody.Position.X > bounds.Right)
+                _velocity.X *= -1;
+
+            if (_dudeBody.Position.Y < bounds.Top || _dudeBody.Position.Y > bounds.Bottom)
+                _velocity.Y *= -1;
+        }
+
+        public void DrawMe(SpriteBatch sb)
+        {
+            _dudeBody.DrawMe(sb);
+            _dudeVisor.DrawMe(sb);
+        }
+    }
+}
